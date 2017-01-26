8f2641c68b18b19fcd77deee46a6b560d9aba520
Jack <jack_6894@hotmail.com>
2017-01-26 12:15:37 +0000

    game now creates to player classes when created

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,18 @@
 require_relative "board"
+require_relative "player"
+
 class Game
 
   attr_reader :board
 
   def initialize
     @board = Board.new
+    @player1 = Player.new
+    @player2 = Player.new
   end
-  
+
+  def players?
+    @player1.nil?
+    @player2.nil?
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,4 +10,14 @@ describe Game do
     expect(game.board).to be_an_instance_of(Board)
   end
 
+  it "should create 2 players when instantiated" do
+    game = Game.new
+    expect(game.players?).to eq(false)
+  end
+
+  it "should have a board with a grid of arrays 3x3" do
+    game = Game.new
+    expect(game.board_ready?).to eq(true)
+  end
+
 end
